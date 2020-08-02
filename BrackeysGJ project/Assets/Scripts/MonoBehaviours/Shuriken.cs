d8ea30bd93b6361a938f456b072afd31c8d86a80
Pierre Gravelle <36173872+Darkforge317@users.noreply.github.com>
2020-08-02 14:53:37 -0600

    Max collisions for Shuriken
    Shuriken will now stop moving after they've collided with 3 objects.

--- BrackeysGJ project/Assets/Scripts/MonoBehaviours/Shuriken.cs
+++ BrackeysGJ project/Assets/Scripts/MonoBehaviours/Shuriken.cs
@@ -11,9 +11,14 @@ public class Shuriken : MonoBehaviour
 
     [SerializeField] private float speed = 300f;
 
+    [SerializeField] private float damageDealt = 10f;
+
     private Rigidbody2D rbody;
 
     private Vector2 moveDirection;
+
+    [SerializeField] private int maxCollisions = 3;
+    private int collisionCount = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +32,7 @@ public class Shuriken : MonoBehaviour
         moveDirection = initMousePos - new Vector2(transform.position.x, transform.position.y);
         moveDirection.Normalize();
 
+        // Set initial velocity
         rbody.velocity = moveDirection * speed * Time.fixedDeltaTime;
     }
 
@@ -38,8 +44,27 @@ public class Shuriken : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if(collisionCount < maxCollisions)
+        {
+            // Maintain velocity in the speed the object is currently traveling in
+            rbody.velocity = speed * (rbody.velocity.normalized) * Time.fixedDeltaTime;
+        }
+        else
+        {
+            rbody.velocity = Vector2.zero;
+        }
+    }
 
-        rbody.velocity = speed * (rbody.velocity.normalized) * Time.fixedDeltaTime;
-        //rbody.velocity = moveDirection * speed * Time.fixedDeltaTime;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collisionCount = collisionCount < maxCollisions ? collisionCount + 1 : maxCollisions;
+        Debug.Log(collisionCount);
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+#warning Chaos, this section needs the enemy's script name and damage method
+            //EnemyScript enemy = collision.gameObject.GetComponent<EnemyScript>();
+            //enemy.TakeDamage(damageDealt);
+        }
     }
 }
