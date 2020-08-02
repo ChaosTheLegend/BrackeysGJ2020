2a7c4493a906f59df04eca4e785b376731e0dee9
Pierre Gravelle <36173872+Darkforge317@users.noreply.github.com>
2020-08-01 23:21:35 -0600

    Added Shuriken Throw
    -Added Prefabs Folder
    - Added Placeholder sprite for Shuriken
    - Shuriken can now be thrown with the left mouse button

---
+++ BrackeysGJ project/Assets/Shuriken.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Shuriken : MonoBehaviour
+{
+    private Vector2 initMousePos;
+    private Vector2 mousePosWorld;
+
+    [SerializeField] private float speed = 10f;
+
+    [SerializeField] private float maxTravelDistance = 100f;
+
+    private Rigidbody2D rbody;
+
+    private Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Mouse's position during shuriken initialization
+        initMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //mousePosWorld = Camera.main.ScreenToWorldPoint(initMousePos);
+
+        rbody = GetComponent<Rigidbody2D>();
+
+        moveDirection = initMousePos - new Vector2(transform.position.x, transform.position.y);
+        moveDirection.Normalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float step = speed * Time.deltaTime;
+
+        //Vector2.MoveTowards(transform.position, new Vector2(40f, 0f), step);        
+        //transform.Translate(transform.forward * Time.deltaTime * speed);
+    }
+
+    private void FixedUpdate()
+    {
+
+        rbody.velocity = moveDirection * speed * Time.fixedDeltaTime;
+    }
+}
---
+++ BrackeysGJ project/Assets/ShurikenThrow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShurikenThrow : MonoBehaviour
+{
+    [SerializeField] private KeyCode shootButton = KeyCode.Mouse0;
+
+    [SerializeField] private GameObject shurikenPrefab;
+    [SerializeField] private GameObject shurikenSpawnPoint;
+    [SerializeField] private GameObject shurikenSpawnParent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(shootButton))
+        {
+            Instantiate(shurikenPrefab, shurikenSpawnPoint.transform.position, Quaternion.identity);
+        }
+    }
+}
