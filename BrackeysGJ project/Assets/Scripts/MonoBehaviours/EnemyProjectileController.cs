ea433f3fe63876077823a7b510582611b75e73cb
bharanidharan27 <55942628+bharanidharan27@users.noreply.github.com>
2020-08-03 15:59:16 +0530

    Added animations to enemy.

--- BrackeysGJ project/Assets/Scripts/MonoBehaviours/EnemyProjectileController.cs
+++ BrackeysGJ project/Assets/Scripts/MonoBehaviours/EnemyProjectileController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using BrackeysGJ.MonoBehaviours;
 using UnityEngine;
 
-public class ShurikenController : MonoBehaviour
+public class EnemyProjectileController : MonoBehaviour
 {
-    [SerializeField] private float shurikenSpeed = 1f;
+    [SerializeField] private float projectileSpeed = 1f;
     [SerializeField] private float damage = 20f;
     private Rigidbody2D rb;
     // private float direction;
@@ -18,7 +17,7 @@ public class ShurikenController : MonoBehaviour
         player = FindObjectOfType<PlayerController>();
         rb = GetComponent<Rigidbody2D>();
         // direction = FindObjectOfType<EnemyController>().GetDirection();
-        direction = (player.transform.position - transform.position) * shurikenSpeed; 
+        direction = (player.transform.position - transform.position) * projectileSpeed; 
         // Vector2 changeInVelocity = new Vector2(direction * shurikenSpeed, 0f);
         Vector2 changeInVelocity = new Vector2(direction.x, direction.y);
         rb.velocity = changeInVelocity;
