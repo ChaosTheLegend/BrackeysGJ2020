9d3cbc377aabe01ea697a7f0a204dc441dd14795
bharanidharan27 <55942628+bharanidharan27@users.noreply.github.com>
2020-08-05 15:51:26 +0530

    Created checkpoints and respawn.

---
+++ BrackeysGJ project/Assets/Scripts/MonoBehaviours/LastCheckpointLocation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LastCheckpointLocation : MonoBehaviour
+{
+    // Should be created only once per level;
+    private static LastCheckpointLocation instance;
+    
+    // Holds the last checkpoint location;
+    public Vector2 LastCheckpointPosition;
+    void Awake()
+    {
+        if(instance == null) {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+        
+    }
+}
