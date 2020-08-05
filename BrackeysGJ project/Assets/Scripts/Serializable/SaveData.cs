2450d75b30f77dcfe00a31b9389f7171a1dd7a01
ChaosTheLegend <chaosthelegend@inbox.ru>
2020-08-05 13:58:06 +0600

    Added savefile management

---
+++ BrackeysGJ project/Assets/Scripts/Serializable/SaveData.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+namespace BrackeysGJ.Serializable
+{
+    [System.Serializable]
+    public class SaveData
+    {
+        private float[] _position;
+        private float[] _camPosition;
+
+        public SaveData(Vector2 playerPos, Vector2 camPos)
+        {
+            _position = new[]{playerPos.x, playerPos.y};
+            _camPosition = new[]{camPos.x, camPos.y};
+        }
+
+        public Vector2 GetPlayerPosition()
+        {
+            return new Vector2(_position[0],_position[1]);
+        }
+
+        public Vector2 GetCameraPosition()
+        {
+            return new Vector2(_camPosition[0],_camPosition[1]);
+        }
+    }
+}
