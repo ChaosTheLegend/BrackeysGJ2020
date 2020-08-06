e81d80ec21ee5d70042b8e63d2b7d9a9e35043db
bharanidharan27 <55942628+bharanidharan27@users.noreply.github.com>
2020-08-06 20:59:44 +0530

    Added Pasue menu buttons.

--- BrackeysGJ project/Assets/Scripts/MonoBehaviours/PauseManager.cs
+++ BrackeysGJ project/Assets/Scripts/MonoBehaviours/PauseManager.cs
@@ -20,5 +20,19 @@ namespace BrackeysGJ.MonoBehaviours
             pausePanel.SetActive(_paused);
             Time.timeScale = _paused ? 0f : 1f;
         }
+
+        public void OnClickingResumeButton()
+        {
+            _paused = !_paused;
+            mixer.TogglePause(_paused);
+            
+            pausePanel.SetActive(_paused);
+            Time.timeScale = _paused ? 0f : 1f;
+        }
+
+        public void OnClickingExitButton()
+        {
+            FindObjectOfType<LevelLoader>().LoadNextScene();
+        }
     }
 }
