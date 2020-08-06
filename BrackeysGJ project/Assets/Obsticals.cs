343993e21131d634a108d521e6333712d971b3c2
bharanidharan27 <55942628+bharanidharan27@users.noreply.github.com>
2020-08-06 11:33:09 +0530

    Added Obsticals.

---
+++ BrackeysGJ project/Assets/Obsticals.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Obsticals : MonoBehaviour
+{
+    private Collider2D obsticalCollider;
+    private PlayerHealth playerHealth; 
+
+    void Start()
+    {
+        obsticalCollider = GetComponent<Collider2D>();
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (obsticalCollider.IsTouchingLayers(LayerMask.GetMask("Player")))
+        {
+            playerHealth.PlayerDied();
+        }
+    }
+}
